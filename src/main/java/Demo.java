d251088be42fb37e424908379a931f56e9506d37
sml <571777232@qq.com>
2020-06-04 22:01:55 +0800

    第二节课笔记

---
+++ src/main/java/Demo.java
@@ -0,0 +1,36 @@
+/**
+ * @Author sml
+ * @Date 2020/6/4 9:48 下午
+ * @Description
+ **/
+
+
+class Father {
+    static {
+        System.out.println("Father静态代码块");
+    }
+
+    {
+        System.out.println("father普通代码块");
+    }
+    public Father() {
+        System.out.println("father构造方法");
+    }
+}
+
+class Son extends Father{
+    static {
+        System.out.println("son静态代码块");
+    }
+    {
+        System.out.println("son普通代码块");
+    }
+    public Son() {
+        System.out.println("son构造方法");
+
+    }
+
+    public static void main(String[] args) {
+        Son son = new Son();
+    }
+}
